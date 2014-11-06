313373930dcf7637f12bae1debc766a7ff8d47e1
asus I7 <asus I7@asus>
2014-11-06 21:12:14 +0100

    sóc una calculadora científica

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,6 +1,6 @@
 
 public class Calculadora {
 	public static void main (String [] args) {
-		System.out.println("Sóc una calculadora simple!");
+		System.out.println("Sóc una calculadora científica!");
 	}
 }
